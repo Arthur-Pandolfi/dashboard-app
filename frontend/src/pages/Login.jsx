6d393f0d1b4650005169ea1c2b23ef837300dda1
Arthur-Pandolfi <arthurpandolfi.dev@gmail.com>
2025-02-20 19:14:52 -0300

    Funcionabilidade de fazer request sobre a senha e o usuário/login com o que foi digitado nso campos adicionada

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
 import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [isDisabled, setIsDisabled] = useState(false);
+  const userInput = useRef(null);
+  const passwordInput = useRef(null);
   async function submitLogin() {
     try {
-      const response = await axios.post("http://localhost:5000/api/login/", {user: "Test", password: "1234"})
+      const response = await axios.post("http://localhost:5000/api/login/", {user: userInput.current.value, password: passwordInput.current.value});
       
       if (response.data.message === "Login successful") {
         navigate("/home")
-      }
+      } 
       
       } catch (error) {
-        console.log(error)
+        setIsDisabled(true)
+        console.log("Usuario não encontrado")
     }
 
   }
@@ -24,12 +29,13 @@ const Login = () => {
           <h1 className="login__tittle">Login</h1>
           <div className="login__fiels">
             <label className="login__user-label" htmlFor="email">Usuário</label>
-            <input className="login__user-input" type="email" placeholder="User123"/>
+            <input className="login__user-input" ref={userInput} type="email" placeholder="User123"/>
             <br />
             <label className="login__password-label" htmlFor="password">Senha</label>
-            <input className="login__password-input" type="password" placeholder="Password123"/>
+            <input className="login__password-input" ref={passwordInput} type="password" placeholder="Password123"/>
             <br/>
             <button className="login__submit-button" type="button" onClick={submitLogin}>Entrar</button>
+            <p className={isDisabled ? "login__error" : "hidden"}>Usuário não encontrado</p>
           </div>
         </div>
       </div>
